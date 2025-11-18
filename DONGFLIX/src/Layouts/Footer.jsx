5a350527e2e20b036bbc873ee0ac6917f56a5e70
EangDev <eangdev.gamedev@gmail.com>
2025-11-18 11:04:44 +0700

    updating footer

--- DONGFLIX/src/Layouts/Footer.jsx
+++ DONGFLIX/src/Layouts/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from '../assets/Logo/mylogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShield, faFileShield, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
@@ -12,21 +14,23 @@ export default function Footer() {
                         className="h-12 mb-4 w-36 rounded-xl"
                     />
                     <p className="text-sm text-center text-gray-400 md:max-w-xs md:text-left">
-                        &copy; {new Date().getFullYear()} Dongflix – Watch Online: Chinese Anime / Donghua. All Rights Reserved
+                        &copy; {new Date().getFullYear()} Dongflix – Watch Online: Chinese Anime / Donghua. 
+                        <br/>All Rights Reserved
                     </p>
                 </div>
-                <div className="flex flex-col space-y-2 text-sm text-gray-400 md:max-w-lg">
+                <div className="flex flex-col space-y-3 text-sm text-[#ccccccc] mr-15 md:max-w-lg">
                     <p>
-                        Disclaimer: This site Dongflix – Watch Online: Chinese Anime / Donghua does not store any files on its server. All contents are provided by non-affiliated third parties.
+                        Disclaimer: This site Dongflix – Watch Online: Chinese Anime / Donghua does not store any files on its server. 
+                        <br/>All contents are provided by non-affiliated third parties.
                     </p>
-                    <p>
+                    <p className='text-base'>
                         Designed with ❤️ by Dongflix Team.
                     </p>
                 </div>
-                <div className="flex flex-col items-center space-y-8 text-sm text-gray-400 md:items-end">
-                    <a href="#" className="transition-colors hover:text-white">Privacy Policy</a>
-                    <a href="#" className="transition-colors hover:text-white">Terms of Service</a>
-                    <a href="#" className="transition-colors hover:text-white">Contact Us</a>
+                <div className="flex flex-col items-center space-y-8 text-sm text-[#cccccc] md:items-start">
+                    <a href="#" className="transition-colors hover:text-[#cccccc]"><FontAwesomeIcon icon={faShield} /> Privacy Policy</a>
+                    <a href="#" className="transition-colors hover:text-[#cccccc]"><FontAwesomeIcon icon={faFileShield} /> Terms of Service</a>
+                    <a href="#" className="transition-colors hover:text-[#cccccc]"><FontAwesomeIcon icon={faEnvelope} /> Contact Us</a>
                 </div>
             </div>
         </footer>
